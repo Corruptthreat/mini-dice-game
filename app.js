b51a9861f3872b9cf1b5ed404ff7b81e2a30e595
Corruptthreat <drawte786@gmail.com>
2019-07-16 18:05:55 +0530

    js updated

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ roundScores = 0;
 activePlayer = 1;
 
 
-
-
 document.querySelector('.dice').style.display='none';
+document.getElementById(score-0).textContent = '0';
+document.getElementById(score-1).textContent = '0';
+document.getElementById(current-0).textContent = '0';
+document.getElementById(current-1).textContent = '0';
 
-document.querySelector('btn-roll').addEventListener('click',function (){
-    
+document.querySelector('.btn-roll').addEventListener('click',function (){
+   
    var dice = Math.floor(Math.random()*6)+1;
-   document.querySelector('.dice').style.display='block';
-    
+   var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display='block';
+  diceDOM.src =  'dice-'+ dice + '.png';
 });
+
